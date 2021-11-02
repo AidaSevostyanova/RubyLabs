86e588cf1565cb2fcf66968e76981880430eef27
Aida Sevostyanova <sevostyanova-aida@mail.ru>
2021-11-02 22:44:23 +0300

    changes in output

--- lab1.rb
+++ lab1.rb
@@ -6,15 +6,15 @@ def greeting
   puts "Введите свой возраст: "
   age = gets.to_i
   if age < 18
-    puts "Привет, " + firstname + " " + lastname + ". Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
-  else puts "Привет, " + firstname + " " + lastname + ". Самое время заняться делом!"
+    p "Привет, " + firstname + " " + lastname + ". Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+  else p "Привет, " + firstname + " " + lastname + ". Самое время заняться делом!"
   end
 end
 
 def foobar(num1, num2)
   if num1 == 20 || num2 == 20
     return num2
-  else puts "Сумма двух чисел: " + (num1+num2).to_s
+  else p "Сумма двух чисел: " + (num1+num2).to_s
   end
 end
 

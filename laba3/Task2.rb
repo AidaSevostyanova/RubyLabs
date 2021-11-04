f5589414746b8690b35979456a1e3cf087b67155
Aida Sevostyanova <sevostyanova-aida@mail.ru>
2021-11-04 12:03:26 +0300

    add Task2

---
+++ laba3/Task2.rb
@@ -0,0 +1,20 @@
+STAFF_LIST_PATH = "laba3/Staff.txt"
+RESULTS_LIST_PATH = "laba3/results.txt"
+
+def task2
+  staff = []
+  File.foreach(STAFF_LIST_PATH) { |employee| staff<<(employee.chomp) }
+  loop do
+    break if staff.size==0
+    puts "Введите возраст: "
+    age=gets.to_i
+    break if age==-1
+    for employee in staff
+      File.write(RESULTS_LIST_PATH, "#{employee}\n", mode: "a") if employee.include?(age.to_s)
+      staff.delete(employee) if employee.include?(age.to_s)
+    end
+  end
+  File.foreach(RESULTS_LIST_PATH) { |employee| puts employee }
+end
+
+task2

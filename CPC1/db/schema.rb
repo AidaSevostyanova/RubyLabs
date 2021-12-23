34b811e1034bff089216dcf105845d23509e8bde
Aida Sevostyanova <sevostyanova-aida@mail.ru>
2021-12-23 15:15:40 +0300

    Add test

--- CPC1/db/schema.rb
+++ CPC1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_150015) do
+ActiveRecord::Schema.define(version: 2021_11_03_180103) do
 
   create_table "labs", force: :cascade do |t|
     t.string "title"
